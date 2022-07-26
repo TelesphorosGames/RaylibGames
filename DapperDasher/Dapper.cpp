30e10abf10f2cd2a7fff6992bded3c4f8ff975e2
C.Teles <telesphorosvids@gmail.com>
2022-07-26 17:56:22 -0600

    "was trying to load the texture before initwindow"

--- DapperDasher/Dapper.cpp
+++ DapperDasher/Dapper.cpp
@@ -4,19 +4,24 @@ int main()
 {
     const int Width = 512;
     const int Height = 380;
+    InitWindow(Width, Height, "Dapper Dasher");
+    SetTargetFPS(60);
 
+
+    
     int Velocity = {0};
     int JumpVelocity = {-22};
     const int Gravity = {1};
 
     bool bIsInAir = false;
 
+    Texture2D Scarfy;
+    
+    Scarfy = LoadTexture("textures/scarfy.png");
 
-
-    Texture2D ScarfyCharacter = LoadTexture("textures/scarfy.png");
     Rectangle ScarfyBoundsRectangle;
-    ScarfyBoundsRectangle.width = ScarfyCharacter.width/6;
-    ScarfyBoundsRectangle.height = ScarfyCharacter.height;
+    ScarfyBoundsRectangle.width = Scarfy.width/6;
+    ScarfyBoundsRectangle.height = Scarfy.height;
     ScarfyBoundsRectangle.x = 0;
     ScarfyBoundsRectangle.y = 0;
 
@@ -24,18 +29,15 @@ int main()
     ScarfyPosition.x = Width/2 - ScarfyBoundsRectangle.width/2;
     ScarfyPosition.y = Height - ScarfyBoundsRectangle.height;
 
-    InitWindow(Width, Height, "Dapper Dasher");
-    SetTargetFPS(60);
+    
 
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        
+       DrawTextureRec(Scarfy, ScarfyBoundsRectangle, ScarfyPosition, WHITE);
 
-       
-       
         if(ScarfyPosition.y >= Height - ScarfyBoundsRectangle.height)
         {
             Velocity = 0;
@@ -71,7 +73,8 @@ int main()
         EndDrawing();
     }
 
-    UnloadTexture(ScarfyCharacter);
+    
+    UnloadTexture(Scarfy);
     CloseWindow();
 
 }
